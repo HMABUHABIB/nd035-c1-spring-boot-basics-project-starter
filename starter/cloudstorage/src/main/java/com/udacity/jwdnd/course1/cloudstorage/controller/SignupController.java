a910faef7044854948178111f4bcf45900ad0dff
abuh004 <husam.abuhabib@arvato.scs.com>
2023-07-02 12:00:30 +0200

    Add RedirectView to singup page

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -3,11 +3,12 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller()
 @RequestMapping("/signup")
@@ -25,7 +26,7 @@ public class SignupController {
     }
 
     @PostMapping()
-    public String signupUser(@ModelAttribute User user, Model model) {
+    public RedirectView signupUser(@ModelAttribute User user, RedirectAttributes attributes) {
         String signupError = null;
 
         if (!userService.isUsernameAvailable(user.getUserName())) {
@@ -40,11 +41,11 @@ public class SignupController {
         }
 
         if (signupError == null) {
-            model.addAttribute("signupSuccess", true);
+            attributes.addFlashAttribute("signupSuccess", true);
         } else {
-            model.addAttribute("signupError", signupError);
+            attributes.addFlashAttribute("signupError", signupError);
         }
 
-        return "login";
+        return new RedirectView("/login",true);
     }
 }
